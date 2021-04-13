1eb2ab144015d9388f9fc05ed19651bb27eb76e3
Mihaela Cocean <mihaela.cocean@verizonmedia.com>
2021-04-13 22:06:46 +0200

    Added login and authentication/authorization validations

--- src/main/java/com/mcbank/service/UserServiceImpl.java
+++ src/main/java/com/mcbank/service/UserServiceImpl.java
@@ -1,8 +1,12 @@
 package com.mcbank.service;
 
+import com.mcbank.exception.AuthenticationException;
 import com.mcbank.model.Account;
+import com.mcbank.model.Credentials;
 import com.mcbank.model.User;
 import com.mcbank.persistence.UserRepository;
+import com.mcbank.service.exception.UserNotFoundException;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,6 +31,27 @@ public class UserServiceImpl implements UserService {
     return userRepository.existsById(id);
   }
 
+  @Override
+  public String login(Credentials credentials) throws UserNotFoundException {
+    List<User> users = userRepository
+        .findByUsernameAndPassword(credentials.getUsername(), credentials.getPassword());
+    if (users.isEmpty()) {
+      throw new UserNotFoundException("Invalid credentials");
+
+    }
+    return users.get(0).getAuthorization();
+  }
+
+  @Override
+  public User authenticateUser(String authorizationHeader)  {
+    List<User> users = userRepository.findByAuthorization(authorizationHeader);
+    if (users.isEmpty()) {
+      throw new AuthenticationException("Invalid user");
+    } else {
+      return users.get(0);
+    }
+  }
+
   void calculateFinalBalance(User user) {
     double balance = 0;
     for (Account account : user.getAccounts()) {
@@ -34,5 +59,5 @@ public class UserServiceImpl implements UserService {
     }
     user.setBalance(balance);
   }
-  
+
 }
