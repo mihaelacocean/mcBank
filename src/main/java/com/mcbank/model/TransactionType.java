521548606d49b7f5027dd65a4ccf454515ad8214
Mihaela Cocean <mihaela.cocean@verizonmedia.com>
2021-04-15 17:06:12 +0200

    Refactored account types and transaction types

--- src/main/java/com/mcbank/model/TransactionType.java
+++ src/main/java/com/mcbank/model/TransactionType.java
@@ -2,17 +2,6 @@ package com.mcbank.model;
 
 public enum TransactionType {
 
-  WITHDRAW(1), ADDING(2);
-
-  private int transactionCode;
-
-   TransactionType(int transactionCode) {
-    this.transactionCode = transactionCode;
-  }
-
-  public int getTransactionCode() {
-     return transactionCode;
-  }
-
+  ADDING, WITHDRAW;
 
 }
